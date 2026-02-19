669bbb3633afe468b36fe9427c68d6eb43b10cfe
mh-rahat <mhrahat765@gmail.com>
2026-02-19 22:47:14 +0600

    add calculate area of square codes

---
+++ C/Chapter1/question1.c
@@ -0,0 +1,16 @@
+// Write a program to calculate area of a square. (side is given)
+
+// area of square 
+
+#include<stdio.h>
+
+int main() {
+    float side;
+    printf("Enter side of this area : " );
+    scanf("%f", &side);
+
+    printf("square of this area : %f", side * side);
+    
+    return 0;
+
+}
