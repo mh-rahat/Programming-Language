e4dab5a509feb8bdfe9e7b2a8408808fe2ac5d26
mh-rahat <mhrahat765@gmail.com>
2026-02-19 21:32:29 +0600

    Added C Programming Codes

---
+++ C/Chapter1/variable.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main() {
+
+
+    // this code displays how data types work in C 
+
+    /*
+       This is a multi-line comment 
+
+    */
+    int age = 20; 
+    float pi = 3.14; 
+    char hashtag = '#';
+    printf("age is %d \n", age );
+    printf("The value of pi is %f \n", pi);
+    printf("The hashtag symbol is %c \n", hashtag);
+
+    return 0;
+}
+
+
