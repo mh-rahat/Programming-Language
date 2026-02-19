e683269a360976488ecfa8c9b2f9dbf133a758c3
mh-rahat <mhrahat765@gmail.com>
2026-02-19 22:13:56 +0600

    add input output file

---
+++ C/Chapter1/input_output.c
@@ -0,0 +1,20 @@
+
+
+#include<stdio.h>
+
+int main() {
+    int num1, num2;
+    printf("Enter capital number : ");
+    scanf("%d", &num1);
+
+    printf("Enter small number : ");
+    scanf("%d", &num2);
+
+    printf("The sum is : %d \n", num1 + num2 );
+    printf("The difference is : %d \n", num1 - num2);
+    printf("The multiplication is : %d \n", num1 * num2);
+    printf("The division is : %d \n", num1 / num2);
+
+
+    return 0;
+}
