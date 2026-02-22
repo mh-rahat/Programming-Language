63ba6900ec4ed8bdf9e7714f66bd1765f37fb261
mh-rahat <mhrahat765@gmail.com>
2026-02-22 12:15:31 +0600

    update the type declaration instructionis file codes

---
+++ C/Chapter2/type_declaration_instructions.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+    int a = 20; 
+    int b = a; 
+    int c = b * 3; 
+    int d = 2, e;
+
+    int oldAge = 20; 
+    int years = 2; 
+    int newAge = oldAge + years; 
+
+    int x, y, z; 
+    int x = y = z = 4; 
+
+    return 0;
+}
